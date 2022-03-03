5b7a16df75eb77239f1b142c44604c10ffbd54b1
Hong Luc <luclhph12819@fpt.edu.vn>
2022-03-03 13:54:18 +0700

    Update giao diện Main Fragment

--- app/src/main/java/com/example/modelfashion/Fragment/MainFragment.java
+++ app/src/main/java/com/example/modelfashion/Fragment/MainFragment.java
@@ -6,6 +6,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,11 +28,21 @@ public class MainFragment extends Fragment {
     private TabLayout tabLayout;
     private ViewPager2 vpMain, vpSaleMain;
     private CircleIndicator3 ciSale;
+    private Handler handler= new Handler(Looper.getMainLooper());
+    ArrayList<ItemSaleMain> arrItem;
     public MainFragment() {
         // Required empty public constructor
     }
 
-
+    private Runnable runnable= new Runnable() {
+        @Override
+        public void run() {
+            if(vpSaleMain.getCurrentItem()==arrItem.size()-1){
+                vpSaleMain.setCurrentItem(0);
+            } else
+                vpSaleMain.setCurrentItem(vpSaleMain.getCurrentItem()+1);
+        }
+    };
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,12 +58,20 @@ public class MainFragment extends Fragment {
         vpMain = view.findViewById(R.id.vp_main_fm);
         vpSaleMain = view.findViewById(R.id.vp_sale_main_fm);
         ciSale = view.findViewById(R.id.ci_sale_main_fm);
-        ArrayList<ItemSaleMain> arrItem = new ArrayList<>();
+        arrItem = new ArrayList<>();
         arrItem.add(new ItemSaleMain(R.drawable.test_img));
         arrItem.add(new ItemSaleMain(R.drawable.test_img));
         arrItem.add(new ItemSaleMain(R.drawable.test_img));
         VpSaleMainFmAdapter vpSaleMainFmAdapter = new VpSaleMainFmAdapter(arrItem);
         vpSaleMain.setAdapter(vpSaleMainFmAdapter);
+        vpSaleMain.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                handler.removeCallbacks(runnable);
+                handler.postDelayed(runnable,2000);
+            }
+        });
 
         ViewPagerMainFmAdapter viewPagerMainFmAdapter = new ViewPagerMainFmAdapter(getActivity());
         vpMain.setAdapter(viewPagerMainFmAdapter);
