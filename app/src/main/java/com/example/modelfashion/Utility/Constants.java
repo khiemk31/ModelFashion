224c0f9c80f6d66b46a1f57617ab604eb6e7493a
khiemk31 <khiemk31@gmail.com>
2022-03-23 20:27:16 +0700

    'update'

--- app/src/main/java/com/example/modelfashion/Utility/Constants.java
+++ app/src/main/java/com/example/modelfashion/Utility/Constants.java
@@ -12,7 +12,4 @@ public class Constants {
     public static final String KEY_PROFILE_NAME = "KEY_PROFILE_NAME";
     public static final String KEY_PRODUCT_ID = "KEY_PRODUCT_ID";
     public static final String ERROR_MESSAGE = "ERROR_MESSAGE";
-
-
-
 }
