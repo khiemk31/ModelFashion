87d81b2aa5defa7b3e62c5f1722ccbdf7817fd5c
khiemnxph10098 <khiemnxph10098@fpt.edu.vn>
2022-05-19 02:47:07 +0700

    add momo

--- app/src/main/java/com/example/modelfashion/Activity/CartActivity.java
+++ app/src/main/java/com/example/modelfashion/Activity/CartActivity.java
@@ -7,27 +7,45 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.modelfashion.Fragment.CartFragment;
 import com.example.modelfashion.Fragment.CategoryFragment;
 import com.example.modelfashion.R;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import vn.momo.momo_partner.AppMoMoLib;
 
 public class CartActivity extends AppCompatActivity {
     private Bundle info;
     private ImageView img_back_cart;
 
 
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
+        AppMoMoLib.getInstance().setEnvironment(AppMoMoLib.ENVIRONMENT.DEVELOPMENT); // AppMoMoLib.ENVIRONMENT.PRODUCTION
         img_back_cart = findViewById(R.id.img_back_cart);
         img_back_cart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+
             }
         });
         Intent intent = getIntent();
@@ -37,6 +55,7 @@ public class CartActivity extends AppCompatActivity {
         CartFragment cartFragment = new CartFragment();
         cartFragment.setArguments(info);
         replaceFragment(cartFragment);
+
     }
 
     private void replaceFragment(Fragment fm){
@@ -46,7 +65,47 @@ public class CartActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.fl_cart,fm);
         fragmentTransaction.commit();
 
+
     }
+
+    // tra Result cua momo
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e("check", String.valueOf(requestCode));
+
+        if (requestCode == AppMoMoLib.getInstance().REQUEST_CODE_MOMO && resultCode == -1) {
+            if (data != null) {
+                if (data.getIntExtra("status", -1) == 0) {
+
+
+                    if (data.getStringExtra("data") != null && !data.getStringExtra("data").equals("")) {
+                        // TODO:
+                        //check_out();
+                       Log.e("check", "Thanh cong");
+
+                    } else {
+                        Log.e("check", "that bai");
+
+                    }
+                } else if (data.getIntExtra("status", -1) == 1) {
+                    Log.e("check", "That bai");
+                    String message = data.getStringExtra("message") != null ? data.getStringExtra("message") : "Thất bại";
+
+                } else if (data.getIntExtra("status", -1) == 2) {
+
+                } else {
+
+                }
+            } else {
+
+            }
+        } else {
+        }
+    }
+
+
+
 
 
 }
--- app/src/main/java/com/example/modelfashion/Fragment/CartFragment.java
+++ app/src/main/java/com/example/modelfashion/Fragment/CartFragment.java
@@ -1,6 +1,7 @@
 package com.example.modelfashion.Fragment;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
@@ -16,6 +18,7 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.example.modelfashion.Activity.CartActivity;
 import com.example.modelfashion.Adapter.cart.CartAdapter;
 import com.example.modelfashion.Model.response.my_product.CartProduct;
 import com.example.modelfashion.Model.response.my_product.MyProduct;
@@ -27,12 +30,16 @@ import com.example.modelfashion.database.MyProductCart;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Currency;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
+import vn.momo.momo_partner.AppMoMoLib;
 
 public class CartFragment extends Fragment {
     private View initView;
@@ -44,13 +51,26 @@ public class CartFragment extends Fragment {
     private ArrayList<String> arr_product_name = new ArrayList<>();
     private String user_id, total_money;
     private TextView tvTotal;
-    private Button btn_payment;
+    public static Button btn_payment;
     private SwipeRefreshLayout refreshLayout;
 
     private Boolean check_load_successful = false;
     private CompositeDisposable disposable = new CompositeDisposable();
 
     CartAdapter adapter = new CartAdapter();
+
+    //momo
+    private Integer amount = 100000;
+    private String fee = "0";
+    int environment = 0;//developer default
+    private String merchantName = "FShop";
+    private String merchantCode = "MOMOLWUA20220517";
+    private String merchantNameLabel = "FPT plytechnich";
+    private String description = "Ví MoMo";
+
+
+
+
 
 
     public CartFragment() {
@@ -75,7 +95,7 @@ public class CartFragment extends Fragment {
         getProductInCart();
 
         btn_payment.setOnClickListener(v -> {
-
+            requestPayment();
         });
 
         refreshLayout.setOnRefreshListener(() -> {
@@ -168,4 +188,53 @@ public class CartFragment extends Fragment {
         disposable.dispose();
         super.onDestroy();
     }
+
+    // goi request MoMo pay
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void requestPayment() {
+        amount = Integer.parseInt(String.valueOf(adapter.getTotal()));
+        AppMoMoLib.getInstance().setAction(AppMoMoLib.ACTION.PAYMENT);
+        AppMoMoLib.getInstance().setActionType(AppMoMoLib.ACTION_TYPE.GET_TOKEN);
+
+
+        Map<String, Object> eventValue = new HashMap<>();
+
+        //client Required
+        eventValue.put("merchantname", merchantName); //Tên đối tác. được đăng ký tại https://business.momo.vn. VD: Google, Apple, Tiki , CGV Cinemas
+        eventValue.put("merchantcode", merchantCode); //Mã đối tác, được cung cấp bởi MoMo tại https://business.momo.vn
+        eventValue.put("amount", amount); //Kiểu integer
+        eventValue.put("orderId", merchantCode + "-" + UUID.randomUUID().toString()); //uniqueue id cho Bill order, giá trị duy nhất cho mỗi đơn hàng
+        eventValue.put("orderLabel", "Mã đơn hàng"); //gán nhãn
+
+        //client Optional - bill info
+        eventValue.put("merchantnamelabel", merchantNameLabel);//gán nhãn
+        eventValue.put("fee", fee); //Kiểu integer
+        eventValue.put("description", description); //mô tả đơn hàng - short description
+
+        //client extra data
+        eventValue.put("requestId",  merchantCode+"merchant_billId_"+System.currentTimeMillis());
+        eventValue.put("partnerCode", merchantCode);
+//        //Example extra data
+//        JSONObject objExtraData = new JSONObject();
+//        try {
+//            objExtraData.put("site_code", "008");
+//            objExtraData.put("site_name", "CGV Cresent Mall");
+//            objExtraData.put("screen_code", 0);
+//            objExtraData.put("screen_name", "Special");
+//            objExtraData.put("movie_name", "Kẻ Trộm Mặt Trăng 3");
+//            objExtraData.put("movie_format", "2D");
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//        eventValue.put("extraData", objExtraData.toString());
+//
+//        eventValue.put("extra", "");
+        AppMoMoLib.getInstance().requestMoMoCallBack(getActivity(), eventValue);
+    }
+
+
+
+
+
+
 }
