2d294ee5323862b4cc03eea79f8304d68b82a875
khiemnxph10098 <khiemnxph10098@fpt.edu.vn>
2022-05-21 15:36:38 +0700

    up

--- app/src/main/java/com/example/modelfashion/History/ViewHistory/HistoryActivity.java
+++ app/src/main/java/com/example/modelfashion/History/ViewHistory/HistoryActivity.java
@@ -103,7 +103,7 @@ public class HistoryActivity extends AppCompatActivity {
         repository = new Repository(HistoryActivity.this);
         Intent intent = getIntent();
         numberStatus = intent.getIntExtra("numberStatus",4);
-        //user_id = intent.getStringExtra("user_id");
+        user_id = intent.getStringExtra("user_id");
         loadTitleStatus(numberStatus);
        // Toast.makeText(this, ""+user_id, Toast.LENGTH_SHORT).show();
         img_history_back.setOnClickListener(new View.OnClickListener() {
