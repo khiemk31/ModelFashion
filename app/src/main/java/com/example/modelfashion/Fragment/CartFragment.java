70d61eff7cf2d60022755da6b9957ffc89364cd0
khiemnxph10098 <khiemnxph10098@fpt.edu.vn>
2022-07-18 21:42:51 +0700

    change Framelayout to viewpager

--- app/src/main/java/com/example/modelfashion/Fragment/CartFragment.java
+++ app/src/main/java/com/example/modelfashion/Fragment/CartFragment.java
@@ -115,6 +115,17 @@ public class CartFragment extends Fragment {
         }
     };
 
+    public static CartFragment newInstance(String text) {
+
+        CartFragment f = new CartFragment();
+        Bundle b = new Bundle();
+        b.putString("msg", text);
+
+        f.setArguments(b);
+
+        return f;
+    }
+
 
 
 
