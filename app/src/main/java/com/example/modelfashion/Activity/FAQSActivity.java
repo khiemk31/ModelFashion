7628f2af96aa556749786ae3287fc3e1ad61eab4
LucLeHong <65607647+LeHongLuc@users.noreply.github.com>
2022-08-01 15:44:06 +0700

    animation FAQS

--- app/src/main/java/com/example/modelfashion/Activity/FAQSActivity.java
+++ app/src/main/java/com/example/modelfashion/Activity/FAQSActivity.java
@@ -16,18 +16,30 @@ import com.example.modelfashion.R;
 public class FAQSActivity extends AppCompatActivity {
     private RelativeLayout btn_contact;
     private LinearLayout detai_FAQS_contact;
+    private int temp = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faqsactivity);
-        btn_contact= findViewById(R.id.btn_act_faqs_Contact);
-        detai_FAQS_contact= findViewById(R.id.detai_FAQS_contact);
-
+        btn_contact = findViewById(R.id.btn_act_faqs_Contact);
+        detai_FAQS_contact = findViewById(R.id.detai_FAQS_contact);
         //https://github.com/Ramotion/folding-cell-android
+        setListener();
+    }
+
+    public void setListener() {
         Animation animation2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.top_to_bottom);
         btn_contact.setOnClickListener(v -> {
-                    detai_FAQS_contact.setVisibility(View.VISIBLE);
-                    detai_FAQS_contact.startAnimation(animation2);
+
+            if (temp == 0) {
+                detai_FAQS_contact.setVisibility(View.VISIBLE);
+                detai_FAQS_contact.startAnimation(animation2);
+                temp++;
+            } else {
+                detai_FAQS_contact.setVisibility(View.GONE);
+                temp--;
+            }
         });
     }
 }
