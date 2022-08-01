5b54ec7915f4b65200eda906a7497fd35e1d501a
LucLeHong <65607647+LeHongLuc@users.noreply.github.com>
2022-08-01 09:59:10 +0700

    .

---
+++ app/src/main/java/com/example/modelfashion/Activity/FAQSActivity.java
@@ -0,0 +1,17 @@
+package com.example.modelfashion.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.modelfashion.R;
+
+
+public class FAQSActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faqsactivity);
+    }
+}
